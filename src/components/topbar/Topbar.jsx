f0bb6b8bbc461bc9e96bd72acebed53a22a67121
danieltrusca <truscadani@yahoo.com>
2021-06-23 17:27:27 +0300

    presentation page- first version

---
+++ src/components/topbar/Topbar.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import { Person, Mail } from "@material-ui/icons";
+import "./Topbar.scss";
+
+const Topbar = ({ menuOpen, setMenuOpen }) => {
+    return (
+        <div className={"topbar " + (menuOpen && "active")}>
+            <div className="wrapper">
+                <div className="left">
+                    <a href="#intro" className="logo">
+                        genius.
+                    </a>
+                    <div className="itemContainer">
+                        <Person className="icon" />
+                        <span>+44 770 726 473</span>
+                    </div>
+                    <div className="itemContainer">
+                        <Mail className="icon" />
+                        <span>truscadani@yahoo.com</span>
+                    </div>
+                </div>
+                <div className="right">
+                    <div className="hamburger"  onClick={()=>setMenuOpen(!menuOpen)}>
+                        <span className="line1"></span>
+                        <span className="line2"></span>
+                        <span className="line3"></span>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Topbar
